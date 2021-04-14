253d33fcc3742dee50cbf56b1736a13b0e28ae32
GauravB7 <gauravbajaj263@gmail.com>
2021-04-14 12:38:03 +0530

    Components Working

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import {MainComponent} from './main/main.component';
+import {ProductsComponent} from './products/products.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {FeedbackComponent} from './feedback/feedback.component';
+import {LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:MainComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'products',component:ProductsComponent},
+  {path:'aboutUs',component:AboutUsComponent},
+  {path:'contactUs',component:ContactUsComponent},
+  {path:'feedback',component:FeedbackComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenubarComponent } from './menubar/menubar.component';
+
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MenubarComponent,
+    FooterComponent,
+    MainComponent,
+    ProductsComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    FeedbackComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
