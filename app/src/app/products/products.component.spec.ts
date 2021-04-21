acad0c488331adb99362d5b500e5917378503be6
GauravB7 <gauravbajaj263@gmail.com>
2021-04-21 16:50:44 +0530

    Updated products.component.spec.ts

--- app/src/app/products/products.component.spec.ts
+++ app/src/app/products/products.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {HttpClientModule} from '@angular/common/http';
 import { ProductsComponent } from './products.component';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -10,7 +11,8 @@ describe('ProductsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ProductsComponent ],
       imports:[
-        HttpClientModule
+        HttpClientModule,
+        RouterTestingModule
       ]
     })
     .compileComponents();
